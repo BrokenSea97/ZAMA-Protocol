116ba3e52a3a5f0194075d5fec839f8b8a611dae
yangguang <yg@qyccar.com>
2025-07-11 18:21:03 +0800

    first commit by bityG

--- contracts/FHECounter.sol
+++ contracts/FHECounter.sol
@@ -1,33 +1,30 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
-import {FHE, euint32, externalEuint32} from "@fhevm/solidity/lib/FHE.sol";
-import {SepoliaConfig} from "@fhevm/solidity/config/ZamaConfig.sol";
+import { FHE, euint32, externalEuint32 } from "@fhevm/solidity/lib/FHE.sol";
+import { SepoliaConfig } from "@fhevm/solidity/config/ZamaConfig.sol";
 
-/// @title A simple FHE counter contract
+/// @title A simple counter contract
+// 继承自 SepoliaConfig 以启用 FHEVM 支持
 contract FHECounter is SepoliaConfig {
-    euint32 private _count;
+    euint32 _count;
 
     /// @notice Returns the current count
     function getCount() external view returns (euint32) {
         return _count;
     }
 
-    /// @notice Increments the counter by a specified encrypted value.
-    /// @dev This example omits overflow/underflow checks for simplicity and readability.
-    /// In a production contract, proper range checks should be implemented.
+    /// @notice Increments the counter by a specific value
     function increment(externalEuint32 inputEuint32, bytes calldata inputProof) external {
-        euint32 encryptedEuint32 = FHE.fromExternal(inputEuint32, inputProof);
+        euint32 evalue = FHE.fromExternal(inputEuint32, inputProof);
+        _count = FHE.add(_count, evalue);
 
-        _count = FHE.add(_count, encryptedEuint32);
-
+        // 为了授予 FHE 权限，我们将调用 FHE.allow() 函数
         FHE.allowThis(_count);
         FHE.allow(_count, msg.sender);
     }
 
-    /// @notice Decrements the counter by a specified encrypted value.
-    /// @dev This example omits overflow/underflow checks for simplicity and readability.
-    /// In a production contract, proper range checks should be implemented.
+    /// @notice Decrements the counter by a specific value
     function decrement(externalEuint32 inputEuint32, bytes calldata inputProof) external {
         euint32 encryptedEuint32 = FHE.fromExternal(inputEuint32, inputProof);
 
